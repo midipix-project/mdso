ffd5c6da2e476586073811c5c66429faa428e3c1
midipix <writeonce@midipix.org>
2016-01-01 03:02:59 -0500

    argv.h: add support for hybrid options (i.e. -std, -pipe).

--- src/skin/mdso_skin_default.c
+++ src/skin/mdso_skin_default.c
@@ -2,32 +2,32 @@
 #include "argv/argv.h"
 
 const struct argv_option mdso_default_options[] = {
-	{"version",		'v',TAG_VERSION,ARGV_OPTARG_NONE,0,0,
+	{"version",		'v',TAG_VERSION,ARGV_OPTARG_NONE,0,0,0,
 				"show version information"},
 
-	{"help",		'h',TAG_HELP,ARGV_OPTARG_OPTIONAL,"short|long",0,
+	{"help",		'h',TAG_HELP,ARGV_OPTARG_OPTIONAL,0,"short|long",0,
 				"show usage information [listing %s options only]"},
 
-	{"machine",		'm',TAG_QUAD_PTR,ARGV_OPTARG_REQUIRED,"32|64",0,
+	{"machine",		'm',TAG_QUAD_PTR,ARGV_OPTARG_REQUIRED,0,"32|64",0,
 				"set machine bits to %s"},
 
-	{"libpath",		'l',TAG_LIBPATH,ARGV_OPTARG_REQUIRED,"loader|peb|system32",0,
+	{"libpath",		'l',TAG_LIBPATH,ARGV_OPTARG_REQUIRED,0,"loader|peb|system32",0,
 				"runtime loader should search for the library either "
 				"according to its internal/inherited path (loader), "
 				"or according to the library path in the process PEB block (peb); "
 				"alternatively, the loader may only search for the library "
 				"in the system library directory (system32)."},
 
-	{"libname",		'n',TAG_LIBNAME,ARGV_OPTARG_REQUIRED,0,"<libname>",
+	{"libname",		'n',TAG_LIBNAME,ARGV_OPTARG_REQUIRED,0,0,"<libname>",
 				"set dependency library name to %s"},
 
-	{"dstdir",		'd',TAG_DSTDIR,ARGV_OPTARG_REQUIRED,0,"<dstdir>",
+	{"dstdir",		'd',TAG_DSTDIR,ARGV_OPTARG_REQUIRED,0,0,"<dstdir>",
 				"save generated assembly files under %s"},
 
-	{"pretty",		'p',TAG_PRETTY,ARGV_OPTARG_REQUIRED,"yaml",0,
+	{"pretty",		'p',TAG_PRETTY,ARGV_OPTARG_REQUIRED,0,"yaml",0,
 				"format output for parsing by %s"},
 
-	{"expsyms",		'e',TAG_EXPSYMS,ARGV_OPTARG_NONE,0,0,
+	{"expsyms",		'e',TAG_EXPSYMS,ARGV_OPTARG_NONE,0,0,0,
 				"print exported symbols" },
 
 	{0}
