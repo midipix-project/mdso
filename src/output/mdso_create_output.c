3e816cade32a6fbe24074bd44c209e7aa353dc79
midipix <writeonce@midipix.org>
2016-01-04 07:58:50 -0500

    annual chores.

--- src/output/mdso_create_output.c
+++ src/output/mdso_create_output.c
@@ -1,6 +1,6 @@
 /****************************************************************/
 /*  mdso: midipix dso scavenger                                 */
-/*  Copyright (C) 2015  Z. Gilboa                               */
+/*  Copyright (C) 2015--2016  Z. Gilboa                         */
 /*  Released under GPLv2 and GPLv3; see COPYING.MDSO.           */
 /****************************************************************/
 
