020b76b64ec8c666382a187fcedb3f24f7024f1b
midipix <writeonce@midipix.org>
2016-05-15 20:46:19 -0400

    project: add explicit rules for object dependency on version.tag.

--- project/extras.mk
+++ project/extras.mk
@@ -1,3 +1,6 @@
 CFLAGS_SHARED_ATTR	+= -DMDSO_PRE_ALPHA -DMDSO_BUILD
 CFLAGS_STATIC_ATTR	+= -DMDSO_PRE_ALPHA -DMDSO_STATIC
 CFLAGS_APP_ATTR		+= -DMDSO_APP
+
+src/driver/mdso_driver_ctx.o:	version.tag
+src/driver/mdso_driver_ctx.lo:	version.tag
