b443cb2693f1cd263c241c4638f17fa00821ffdd
midipix <writeonce@midipix.org>
2021-07-06 16:18:18 +0000

    Annual chores.

--- src/logic/mdso_asmgen_symentry.c
+++ src/logic/mdso_asmgen_symentry.c
@@ -1,6 +1,6 @@
 /****************************************************************/
 /*  mdso: midipix dso scavenger                                 */
-/*  Copyright (C) 2015--2017  Z. Gilboa                         */
+/*  Copyright (C) 2015--2021  Z. Gilboa                         */
 /*  Released under GPLv2 and GPLv3; see COPYING.MDSO.           */
 /****************************************************************/
 
